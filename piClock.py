51148e2b3615ceaab78c5ce6c6e8ea13ebc299f7
demethan <llevesque@gmail.com>
2020-02-28 07:55:31 -0500

    fixed the hour turn over.

--- piClock.py
+++ piClock.py
@@ -1,42 +1,71 @@
 import sys
-from tkinter import *
 import datetime
-import time
+import os,time
 import board
 import busio
 import adafruit_is31fl3731
+from loguru import logger
 
-i2c = busio.I2C(board.SCL, board.SDA)
-display = adafruit_is31fl3731.Matrix(i2c)
-led_level = 255
+class Clock():
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.display = adafruit_is31fl3731.Matrix(self.i2c)
+        self.led_level = 255
+        self.clear = lambda: os.system('clear')
 
-pos_dict = {}
-counter = 1
-for x in range(1,9):
-    for y in range(1,16):
-        pos_dict[counter] ={"x": x,"y":y}
-        counter += 1
- 
+        self.pos_dict = {}
+        counter = 0
+        for x in range(0,16):
+            for y in range(0,9):
+                self.pos_dict[counter] ={"x": x,"y":y}
+                counter += 1
+                self.display.pixel(x,y,127)
+                time.sleep(.05)
+        #test LEDs
+        self.display.fill(255)
+        time.sleep(.2)
+        self.display.fill(0)
+        self.previous_hour_led = 0 
+        self.previous_min_led = 0
 
-#get the time and drive the led
-def tick():
-    now = datetime.datetime.now()
-    led_a_level = round(led_level * (now.second/60),0)
-    led_b_level = led_level - led_a_level
-    hour = now.hour +60
-    minute = now.minute
+
+
+
+    #get the time and drive the led
+    def tick(self):
+        self.clear()
+        now = datetime.datetime.now()
+        led_a_level = int(round(self.led_level * (now.second/60),0))
+        led_b_level = self.led_level - led_a_level
+        hour = now.hour + 60
+        minute = now.minute
+        print(now.hour,now.minute, now.second," ",hour,minute)
+        try:
+            if hour > 72:
+                hour = hour -12
+
+            self.display.pixel(self.pos_dict[self.previous_hour_led]["x"],self.pos_dict[self.previous_hour_led]["y"],0) #turn off previous LED
+            self.display.pixel(self.pos_dict[hour]["x"],self.pos_dict[hour]["y"],self.led_level) #displaying hour LED
+            self.previous_hour_led = hour
+                    
+            self.display.pixel(self.pos_dict[self.previous_min_led]["x"],self.pos_dict[self.previous_min_led]["y"],0) #turn off previous minute LED
+            self.display.pixel(self.pos_dict[minute]["x"],self.pos_dict[minute]["y"],led_b_level) #Current minute LED
+            if minute < 59:
+                self.display.pixel(self.pos_dict[minute+1]["x"],self.pos_dict[minute+1]["y"],led_a_level) #Next minute LED
+            else:
+                self.display.pixel(self.pos_dict[0]["x"],self.pos_dict[0]["y"],led_a_level) #Next minute LED
+            self.previous_min_led = minute
+            
+            msg = "x:"+str(self.pos_dict[hour]["x"])+" y:"+str(self.pos_dict[hour]["y"])+"\n"
+            msg += "x:"+str(self.pos_dict[minute]["x"])+" y:"+str(self.pos_dict[minute]["y"])+" lvl:"+str(led_b_level)+"\n"
+            print(msg)
+        except Exception as err:
+            logger.exception(err)
     
-    display.pixel(pos_dict[hour]["x"],pos_dict[hour]["y"],led_level) #displaying hour
-    if minute < 59:
-        display.pixel(pos_dict[minute]["x"],pos_dict[minute]["y"],led_a_level)
-        display.pixel(pos_dict[minute+1]["x"],pos_dict[minute+1]["y"],led_b_level)
-    else
-        display.pixel(pos_dict[minute]["x"],pos_dict[minute]["y"],led_a_level)
-        display.pixel(pos_dict[minute-59]["x"],pos_dict[minute-59]["y"],led_b_level)
+    
 
 
-
-
-while true:
-    tick()
+clock = Clock()
+while True:
+    clock.tick()
     time.sleep(1)
