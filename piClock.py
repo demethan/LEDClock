0e2d7fed31998f1655d118fab34e191ff96984d4
demethan <llevesque@gmail.com>
2020-02-28 18:10:29 -0500

    move display to own functionn, added side A and B of clock

--- piClock.py
+++ piClock.py
@@ -11,16 +11,16 @@ class Clock():
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.display = adafruit_is31fl3731.Matrix(self.i2c)
         self.led_level = 255
-        self.clear = lambda: os.system('clear')
+        self.clear = lambda: os.system('clear') 
 
         self.pos_dict = {}
         counter = 0
-        for x in range(0,16):
+        for x in range(0,16):   #address the LED matrix to a int. 0={0,0},1={0,1}, etc
             for y in range(0,9):
                 self.pos_dict[counter] ={"x": x,"y":y}
                 counter += 1
-                self.display.pixel(x,y,127)
-                time.sleep(.05)
+                self.display.pixel(x,y,127) #turn on LED at that possition. 
+                time.sleep(.01)
         #test LEDs
         self.display.fill(255)
         time.sleep(.2)
@@ -37,23 +37,25 @@ class Clock():
         now = datetime.datetime.now()
         led_a_level = int(round(self.led_level * (now.second/60),0))
         led_b_level = self.led_level - led_a_level
-        hour = now.hour + 60
+        if now.hour > 12:
+            offset = 48 #conver 24h to 12
+        else:
+            offset = 60
+        hour = now.hour + offset #offset for the led matrix, don't want to overlap minute's LEDs (minute LED 0-59, Hour LED 60-71).
         minute = now.minute
         print(now.hour,now.minute, now.second," ",hour,minute)
+        
         try:
-            if hour > 72:
-                hour = hour -12
-
-            self.display.pixel(self.pos_dict[self.previous_hour_led]["x"],self.pos_dict[self.previous_hour_led]["y"],0) #turn off previous LED
-            self.display.pixel(self.pos_dict[hour]["x"],self.pos_dict[hour]["y"],self.led_level) #displaying hour LED
+            self.light(self.previous_hour_led,0) #turn off previous LED
+            self.light(hour,self.led_level) #displaying hour LED
             self.previous_hour_led = hour
                     
-            self.display.pixel(self.pos_dict[self.previous_min_led]["x"],self.pos_dict[self.previous_min_led]["y"],0) #turn off previous minute LED
-            self.display.pixel(self.pos_dict[minute]["x"],self.pos_dict[minute]["y"],led_b_level) #Current minute LED
+            self.light(self.previous_min_led,0) #turn off previous minute LED
+            self.light(minute,led_b_level) #Current minute LED
             if minute < 59:
-                self.display.pixel(self.pos_dict[minute+1]["x"],self.pos_dict[minute+1]["y"],led_a_level) #Next minute LED
+                self.light(minute+1,led_a_level) #Next minute LED
             else:
-                self.display.pixel(self.pos_dict[0]["x"],self.pos_dict[0]["y"],led_a_level) #Next minute LED
+                self.light(0,led_a_level) #Next minute LED
             self.previous_min_led = minute
             
             msg = "x:"+str(self.pos_dict[hour]["x"])+" y:"+str(self.pos_dict[hour]["y"])+"\n"
@@ -62,7 +64,10 @@ class Clock():
         except Exception as err:
             logger.exception(err)
     
-    
+    def light(self, pos, level):
+        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #clock side A
+        pos = pos + 72
+        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #Clock sido B
 
 
 clock = Clock()
