69f50e3ef50f1dfe4dc67f39556c084c7aef8fce
demethan <llevesque@gmail.com>
2020-03-07 22:19:44 -0500

    added light sensor for night mode

--- piClock.py
+++ piClock.py
@@ -5,13 +5,18 @@ import board
 import busio
 import adafruit_is31fl3731
 from loguru import logger
+import RPi.GPIO as GPIO
+import time
 
 class Clock():
     def __init__(self):
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.display = adafruit_is31fl3731.Matrix(self.i2c)
-        self.led_level = 255
-        #self.clear = lambda: os.system('clear') 
+        #self.led_level = 255
+        GPIO.setmode(GPIO.BCM) #for light sensor
+        self.a_pin = 18
+        self.b_pin = 23
+
 
         self.pos_dict = {}
         counter = 0
@@ -29,25 +34,51 @@ class Clock():
         self.previous_min_led = 0
 
 
+    # create discharge function for reading capacitor data
+    def discharge(self):
+        GPIO.setup(self.a_pin, GPIO.IN)
+        GPIO.setup(self.b_pin, GPIO.OUT)
+        GPIO.output(self.b_pin, False)
+        time.sleep(0.005)
 
+    # create time function for capturing analog count value
+    def charge_time(self):
+        GPIO.setup(self.b_pin, GPIO.IN)
+        GPIO.setup(self.a_pin, GPIO.OUT)
+        count = 0
+        GPIO.output(self.a_pin, True)
+        while not GPIO.input(self.b_pin):
+            count = count +1
+        return count
+
+    # create analog read function for reading charging and discharging data
+    def analog_read(self):
+        self.discharge()
+        return self.charge_time()
 
     #get the time and drive the led
     def tick(self):
         #elf.clear()
+        ambiant_light = self.analog_read()
+        led_level = int(round(((500 - ambiant_light)/450*255),0))
+        if led_level > 255: 
+            led_level = 255 #day mode
+        if led_level < 25:
+            led_level = 25 #night mode
+        print(led_level)
         now = datetime.datetime.now()
-        led_a_level = int(round(self.led_level * (now.second/60),0))
-        led_b_level = self.led_level - led_a_level
+        led_a_level = int(round(led_level * (now.second/60),0))
+        led_b_level = led_level - led_a_level
         if now.hour >= 12:
             offset = 48 #convert 24h to 12
         else:
             offset = 60
         hour = now.hour + offset #offset for the led matrix, don't want to overlap minute's LEDs (minute LED 0-59, Hour LED 60-71).
         minute = now.minute
-        #print(now.hour,now.minute, now.second," ",hour,minute) #for debug
         
         try:
             self.light(self.previous_hour_led,0) #turn off previous LED
-            self.light(hour,self.led_level) #displaying hour LED
+            self.light(hour,led_level) #displaying hour LED
             self.previous_hour_led = hour
                     
             self.light(self.previous_min_led,0) #turn off previous minute LED
@@ -58,9 +89,6 @@ class Clock():
                 self.light(0,led_a_level) #Next minute LED
             self.previous_min_led = minute
             
-            """ msg = "x:"+str(self.pos_dict[hour]["x"])+" y:"+str(self.pos_dict[hour]["y"])+"\n"
-            msg += "x:"+str(self.pos_dict[minute]["x"])+" y:"+str(self.pos_dict[minute]["y"])+" lvl:"+str(led_b_level)+"\n"
-            print(msg) #debug """
         except Exception as err:
             logger.exception(err)
     
