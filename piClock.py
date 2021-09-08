97107a4c0b80b0d793d632000e086aa435f60cd4
demethan <llevesque@gmail.com>
2021-09-07 22:32:22 -0400

    working with full minutes lit.

--- piClock.py
+++ piClock.py
@@ -56,9 +56,21 @@ class Clock():
         self.discharge()
         return self.charge_time()
 
+    def light(self, pos, level):
+        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #clock side A
+        pos = pos + 72
+        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #Clock sido B
+
+    def animate(self):
+        self.light(0,255)
+        for x in range(0,60):   #address the LED matrix to a int. 0={0,0},1={0,1}, etc
+            self.light(x,0)
+            time.sleep(.0166)
+
+
     #get the time and drive the led
     def tick(self):
-        #elf.clear()
+        #self.clear()
         ambiant_light = self.analog_read()
         led_level = int(round(((500 - ambiant_light)/450*255),0))
         if led_level > 255: 
@@ -66,42 +78,56 @@ class Clock():
         if led_level < 25:
             led_level = 25 #night mode
         now = datetime.datetime.now()
-        led_a_level = int(round(led_level * (now.second/60),0))
-        led_b_level = led_level - led_a_level
+        #led_a_level = int(round(led_level * (now.second/60),0))
+        #led_b_level = led_level - led_a_level
         if now.hour >= 12:
             offset = 48 #convert 24h to 12
         else:
             offset = 60
         hour = now.hour + offset #offset for the led matrix, don't want to overlap minute's LEDs (minute LED 0-59, Hour LED 60-71).
         minute = now.minute
+        second = now.second
         
         try:
-            self.light(self.previous_hour_led,0) #turn off previous LED
-            self.light(hour,led_level) #displaying hour LED
-            self.previous_hour_led = hour
-                    
-            self.light(self.previous_min_led,0) #turn off previous minute LED
-            self.light(minute,led_b_level) #Current minute LED
+            #minutes        
+            #Light minut and keep minute lit
             if minute < 59:
-                self.light(minute+1,led_a_level) #Next minute LED
+                self.light(minute,led_level) #Light minute and
+                #hour
+                
+                
+
+            else: 
+                if second < 59:
+                    self.light(minute,led_level) #Light minute and
+                    self.light(0,0) #turn off minute at 12 mark for readability. 
+                    #hour
+                    self.light(hour,led_level) #displaying hour LED
+
+                else:#reset LED for next our.
+                    self.animate()
+                    self.light(0,led_level) #minute LED
+                    hour = now.hour + offset
+                    self.light(hour,led_level) #hour led
+                    
+                    
+            #print(hour)
+            if(hour==60):
+                self.light(71,0) # when converting from 24h to 12h. need to turn off previous
             else:
-                self.light(0,led_a_level) #Next minute LED
-            self.previous_min_led = minute
+                self.light(hour-1,0) #previous hour
+            self.light(hour,led_level) #displaying hour LED
+                
             
         except KeyboardInterrupt:
             self.display.fill(0)
         
         except Exception as err:
             logger.exception(err)
-            
-    
-    def light(self, pos, level):
-        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #clock side A
-        pos = pos + 72
-        self.display.pixel(self.pos_dict[pos]["x"],self.pos_dict[pos]["y"],level) #Clock sido B
+
 
 
 clock = Clock()
 while True:
     clock.tick()
-    time.sleep(1)
+    time.sleep(0.2)
